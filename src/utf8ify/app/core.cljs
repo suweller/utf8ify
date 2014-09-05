6c46887c9e2a513774a99348cd1e756eec22e049
Steven Weller <github@suweller.com>
2014-09-05 15:39:02 +0200

    Filter out draw events, improve UI

--- src/utf8ify/app/core.cljs
+++ src/utf8ify/app/core.cljs
@@ -7,6 +7,10 @@
 (def draw-events (chan))
 
 (go
-  (loop [[msg-name _ :as msg] (<! draw-events)]
-    (.log js/console msg-name)
-    (recur (<! draw-events))))
+  (loop [[msg-name _ :as msg] (<! mouse-events)
+         drawing false]
+    (let [start (= msg-name :start)
+          continue (not (= msg-name :stop))
+          draw (and (or drawing start) continue)]
+      (if draw (>! draw-events msg))
+      (recur (<! mouse-events) draw))))
--- src/utf8ify/web/core.cljs
+++ src/utf8ify/web/core.cljs
@@ -8,24 +8,35 @@
 
 (def grid-size 64)
 (def app-state (atom
-                 {:cells (take grid-size (map (fn [n] {:id n}) (iterate inc 0)))
-                  :mouse-events (chan)
-                  :draw-events (chan)} ))
+                 {:cells (take grid-size (map (fn [n] {:id n :on false}) (iterate inc 0)))}))
 
 (defn grid [data owner]
   (om/component
-    (let [cells (:cells data) mouse-events (:mouse-events data)]
+    (let [cells (:cells data) mouse-events app/mouse-events]
       (apply dom/div
              #js {:className "grid"
                   :onMouseDown #(xy-message mouse-events :start %)
                   :onMouseMove #(xy-message mouse-events :move %)
                   :onMouseUp #(xy-message mouse-events :stop %)
-                  :onMouseOut #(xy-message mouse-events :stop %)}
-             (map (fn [cell] (dom/div #js {:data-id (:id cell) :className "cell"})) cells)))))
-
-(om/root grid @app-state
-              {:target (. js/document (getElementById "app")) })
+                  :onMouseLeave #(xy-message mouse-events :stop %)}
+             (map (fn [cell] (dom/div #js {:data-id (:id cell) :data-on (:on cell) :className "cell"})) cells)))))
 
 (defn xy-message [ch msg-name xy-obj]
   (put! ch [msg-name {:x (.-pageX xy-obj) :y (.-pageY xy-obj)}]))
 
+(om/root grid app-state
+              {:target (. js/document (getElementById "app")) })
+
+(go
+  (loop [[msg-name {x :x y :y}] (<! app/draw-events)]
+    (let [cell (.elementFromPoint js/document x y)
+          data-id (.-id (.-dataset cell))]
+      (.log js/console (str "Cell: " data-id))
+      (activate-cell data-id))
+    (recur (<! app/draw-events))))
+
+(defn activate-cell [id]
+  (swap! app-state (fn [data]
+         (let [cells (:cells data)
+               updated-cells (map (fn [cell] (assoc cell {:on (or (:on cell) (= (:id cell) id))})) cells)]
+           (assoc data {:cells updated-cells})))))
