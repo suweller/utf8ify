aa90ef9fe5442bb68f6e875cf0602a188b2909b0
Steven Weller <github@suweller.com>
2014-09-12 11:33:21 +0200

    Draft dumb pattern matching

--- test/utf8ify/app_test.cljs
+++ test/utf8ify/app_test.cljs
@@ -5,16 +5,80 @@
   (:require-macros [cemerick.cljs.test :refer (is deftest)]
                    [cljs.core.async.macros :as m :refer [go]]))
 
-(deftest dummy
-  (is (= 1 1)))
-; (deftest draw-on-receiving-start
-;   (let [in (chan) out (chan)
-;         filter-draw-events! (fn [in out] )]
-;     (go
-;       (do
-;         (filter-draw-events! in out)
-;         (>! in [:start])
-;         (is [:start _] (<! out))))))
+(def character-ascii-art
+  {"↑" ["...*..."
+        "..***.."
+        ".*.*.*."
+        "...*..."
+        "...*..."
+        "...*..."
+        "...*..."]
 
-; [:start :this :that :stop]
-; [:not-this :start :this :that :stop :not-that]
+   "✝" ["...*..."
+        "...*..."
+        ".*****."
+        "...*..."
+        "...*..."
+        "...*..."
+        "...*..."]
+
+   "␣" ["......."
+        "......."
+        "......."
+        "......."
+        "......."
+        ".*...*."
+        ".*****."]
+
+   "→" ["......."
+        "....*.."
+        ".....*."
+        "*******"
+        ".....*."
+        "....*.."
+        "......."]})
+
+(defn to-bits [ascii-art]
+  (defn ch-to-bit [ch]
+    (if (= "*" ch) 1 0))
+  (flatten
+    (map (fn [line] (map ch-to-bit line))
+         ascii-art)))
+
+(defn char-to-bits [ch]
+  (to-bits (get character-ascii-art ch)))
+
+(def visible-space "␣")
+(def up-arrow "↑")
+(def right-arrow "→")
+(def cross "✝")
+
+(deftest char-to-bits-finds-visible-whitespace
+  (is (= [0 0 0 0 0 0 0
+          0 0 0 0 0 0 0
+          0 0 0 0 0 0 0
+          0 0 0 0 0 0 0
+          0 0 0 0 0 0 0
+          0 1 0 0 0 1 0
+          0 1 1 1 1 1 0] (char-to-bits visible-space))))
+
+(def pattern-map
+  (into {}
+        (map (fn [[ch art]] [(to-bits art) ch])
+             (seq character-ascii-art))))
+
+(defn find-utf8-character [drawing]
+  (get pattern-map drawing))
+
+(deftest find-utf8-char-for-visible-space
+  (is (= visible-space (find-utf8-character (char-to-bits visible-space)))))
+
+(deftest find-utf8-char-for-right-arrow
+  (is (= right-arrow (find-utf8-character (char-to-bits right-arrow)))))
+
+(deftest find-utf8-char-for-up-arrow
+  (is (= up-arrow (find-utf8-character (char-to-bits up-arrow)))))
+
+(deftest find-utf8-char-for-cross
+  (is (= cross (find-utf8-character (char-to-bits cross)))))
+
